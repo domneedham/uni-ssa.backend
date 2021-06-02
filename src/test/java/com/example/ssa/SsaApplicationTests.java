57bf9e12b26bce81e8c3a4ea53de59c0fc990b23
Dominic Needham <dominic.needham@bt.com>
2021-06-02 09:57:40 +0100

    Initial commit

---
+++ src/test/java/com/example/ssa/SsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
