4e388ef8aff1dd7bf26ed956e266dea10a670eff
Dominic Needham <dominic.needham@bt.com>
2021-08-09 15:04:31 +0100

    Category controller tests made

--- src/main/java/com/example/ssa/web/api/CategoryController.java
+++ src/main/java/com/example/ssa/web/api/CategoryController.java
@@ -2,6 +2,7 @@ package com.example.ssa.web.api;
 
 import com.example.ssa.entity.skill.Category;
 import com.example.ssa.entity.skill.Skill;
+import com.example.ssa.exceptions.requests.bad.CategoryDoesNotExistException;
 import com.example.ssa.repository.CategoryRepository;
 import com.example.ssa.repository.SkillRepository;
 import com.example.ssa.repository.StaffSkillRepository;
@@ -36,7 +37,13 @@ public class CategoryController {
 
     @GetMapping("/{id}")
     public Optional<Category> findById(@PathVariable(value = "id") Long id) {
-        return categoryRepository.findById(id);
+        Optional<Category> category = categoryRepository.findById(id);
+
+        if (category.isEmpty()) {
+            throw new CategoryDoesNotExistException("Category not found with that id");
+        }
+
+        return category;
     }
 
     @PostMapping("/create")
@@ -46,12 +53,24 @@ public class CategoryController {
 
     @PutMapping("/update")
     public Category update(@RequestBody Category category) {
+        Optional<Category> categoryToUpdate =  categoryRepository.findById(category.getId());
+
+        if (categoryToUpdate.isEmpty()) {
+            throw new CategoryDoesNotExistException("Category not found with that id");
+        }
+
         return categoryRepository.save(category);
     }
 
     @DeleteMapping("/delete/{id}")
     @Transactional
     public void delete(@PathVariable(value = "id") Long id) {
+        Optional<Category> categoryToDelete = categoryRepository.findById(id);
+
+        if (categoryToDelete.isEmpty()) {
+            throw new CategoryDoesNotExistException("Category not found with that id");
+        }
+
         List<Skill> skills = skillRepository.findAllByCategoryId(id);
         ArrayList<Long> ids = new ArrayList<>();
         skills.forEach(skill -> ids.add(skill.getId()));
---
+++ src/test/java/com/example/ssa/web/api/CategoryControllerTest.java
@@ -0,0 +1,201 @@
+package com.example.ssa.web.api;
+
+import com.example.ssa.entity.skill.Category;
+import com.example.ssa.entity.skill.Skill;
+import com.example.ssa.entity.skill.StaffSkill;
+import com.example.ssa.entity.user.AppUser;
+import com.example.ssa.entity.user.UserRole;
+import com.example.ssa.exceptions.requests.bad.CategoryDoesNotExistException;
+import com.example.ssa.repository.CategoryRepository;
+import com.example.ssa.repository.SkillRepository;
+import com.example.ssa.repository.StaffSkillRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CategoryController.class)
+public class CategoryControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    CategoryRepository categoryRepository;
+    @MockBean
+    SkillRepository skillRepository;
+    @MockBean
+    StaffSkillRepository staffSkillRepository;
+
+    Category categoryOne = new Category(1L, "Test Category One", 26932);
+    Category categoryTwo = new Category(2L, "Test Category Two", 19165);
+
+    Skill skillOne = new Skill(1L, "Skill One", categoryOne);
+
+    AppUser appUserOne = new AppUser(1L, "Test", "User", "test@user.com", UserRole.STAFF, "Test User");
+
+    StaffSkill staffSkillOne = new StaffSkill(1L, skillOne, appUserOne, 5, LocalDateTime.now(), LocalDateTime.now().plusDays(30));
+
+    @Test
+    public void findAllCategories_success() throws Exception {
+        List<Category> records = new ArrayList<>(List.of(categoryOne, categoryTwo));
+
+        when(categoryRepository.findAll()).thenReturn(records);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/category/")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name", is(categoryOne.getName())));
+    }
+
+    @Test
+    public void findAllCategories_empty() throws Exception {
+        List<Category> records = new ArrayList<>(List.of());
+
+        when(categoryRepository.findAll()).thenReturn(records);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/category/")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    public void findById_success() throws Exception {
+        when(categoryRepository.findById(1L)).thenReturn(java.util.Optional.ofNullable(categoryOne));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/category/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.name", is(categoryOne.getName())));
+    }
+
+    @Test
+    public void findById_notFound() throws Exception {
+        when(categoryRepository.findById(1L)).thenReturn(java.util.Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/category/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof CategoryDoesNotExistException))
+                .andExpect(result -> assertEquals("Category not found with that id",
+                        Objects.requireNonNull(result.getResolvedException()).getMessage())
+                );
+    }
+
+    @Test
+    public void createCategory_success() throws Exception {
+        Category category = Category.builder()
+                .name(categoryOne.getName())
+                .icon(categoryOne.getIcon())
+                .build();
+        when(categoryRepository.save(category)).thenReturn(category);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/category/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(category));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.name", is(categoryOne.getName())));
+    }
+
+    @Test
+    public void updateCategory_success() throws Exception {
+        Category category = Category.builder()
+                .id(categoryOne.getId())
+                .name(categoryOne.getName())
+                .icon(categoryOne.getIcon())
+                .build();
+
+        when(categoryRepository.save(category)).thenReturn(category);
+        when(categoryRepository.findById(1L)).thenReturn(java.util.Optional.ofNullable(categoryOne));
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.put("/api/category/update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(category));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.name", is(categoryOne.getName())));
+    }
+
+    @Test
+    public void updateCategory_nullId() throws Exception {
+        Category category = Category.builder()
+                .name(categoryOne.getName())
+                .icon(categoryOne.getIcon())
+                .build();
+
+        when(categoryRepository.save(category)).thenReturn(category);
+        when(categoryRepository.findById(1L)).thenReturn(java.util.Optional.empty());
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.put("/api/category/update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(category));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof CategoryDoesNotExistException))
+                .andExpect(result -> assertEquals("Category not found with that id",
+                        Objects.requireNonNull(result.getResolvedException()).getMessage())
+                );
+    }
+
+    @Test
+    public void deleteCategory_success() throws Exception {
+        // TODO
+        // TEST THE DELETE METHODS ARE CALLED FOR SKILL AND STAFFSKILL REPOSITORIES
+        when(categoryRepository.findById(1L)).thenReturn(java.util.Optional.ofNullable(categoryOne));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/api/category/delete/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteCategory_notFound() throws Exception {
+        // TODO
+        // TEST THE DELETE METHODS ARE NOT CALLED FOR SKILL AND STAFFSKILL REPOSITORIES
+        when(categoryRepository.findById(1L)).thenReturn(java.util.Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/api/category/delete/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof CategoryDoesNotExistException))
+                .andExpect(result -> assertEquals("Category not found with that id",
+                        Objects.requireNonNull(result.getResolvedException()).getMessage())
+                );
+    }
+}
